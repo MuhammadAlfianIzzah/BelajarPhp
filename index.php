431dbbfa29395d9738f1e819941ce00ea76e948f
MuhammadAlfianIzzah <alfianizzah6@gmail.com>
2021-06-19 21:40:49 +0800

    i

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+// true = 1  false 0
+//  % modelus
+//  ** pangkat
+
+//  fungsi  is_int untuk mengecek nilainya int atau bukan
+// is_int return bolean(true false)
+// max() min() ...  return value
+
+//  function(parameter1, parameter2, ...)
+// var_dump(rand(10, 200));
+
+
+// info variabel
+//  awali $ 
+//  media untuk menyimpan data
+
+// $nama = "Alfianizzah";
+// $umur = 50;
+
+// $nama = "Alfianizzah";
+// $umur = 100;
+// $str = "nama saya " . $nama . " " . $umur;
+// define("TITLE", "Aplikasi sekolah terbang");
+// $title = "Aplikasi sekolah terbang";
+// $umur = 20;
+// echo $str;
+// echo  $umur;
+
+// echo $title;
+function buatNama()
+{
+    return "saya suka jalan jalan";
+}
+
+$iniFunc = buatNama();
+echo $iniFunc;
